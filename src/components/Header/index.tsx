23063b1aea0d618a13cf2944267d38c37f654cba
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2021-11-03 22:05:37 -0300

    Projeto Iniciado e Cabeçalho pronto

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Cabecalho, Container, LinkItem, LinkTitle, List, Title } from './styles';
+
+const Header =() =>{
+    return (
+    
+            <Cabecalho>
+                <Container>
+                    <div>
+                        <Title>
+                             <LinkTitle href="#">Blindex Vidros</LinkTitle>
+                         </Title>
+                    </div>
+
+                    <List>
+                        <li><LinkItem href="#quem-somos">Quem Somos</LinkItem></li>
+                        <li><LinkItem href="#servicos">Serviços</LinkItem></li>
+                        <li><LinkItem href="#orcamentos">Orçamentos</LinkItem></li>
+                        <li><LinkItem href="#contato">Contato</LinkItem></li>
+                    </List>
+                </Container>
+            </Cabecalho>
+
+        
+    );
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+
+export const Cabecalho = styled.header`
+    background-color: #C96C61;
+    height: 120px;
+    display: flex;
+    align-items: center;
+    color:#FFF;
+`;
+
+export const Container = styled.div`
+
+    display: flex;
+    max-width: 992px;
+    margin:  auto;
+    width: 100%;
+    min-width: 320px;
+    justify-content: space-between;
+    
+
+
+`;
+
+export const Title = styled.h1`
+
+&:hover {
+    color: #AAA;
+    cursor: pointer;
+}
+`;
+
+export const LinkTitle = styled.a`
+text-decoration: none;
+color: #FFF;
+
+&:hover {
+    color: #AAA;
+}
+`;
+
+export const List = styled.ul`
+    display: flex;
+    width: 100%;
+    max-width: 400px;
+    justify-content: space-between;
+    align-items: center;
+    list-style-type: none;
+`;
+
+export const LinkItem = styled.a`
+color: #fff;
+text-decoration: none;
+
+
+&:hover {
+    color: #AAA;
+}
+`;
+
+
+
