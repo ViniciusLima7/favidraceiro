3e44fa4d7ac3d6d991ce70fae49861354ea6c07c
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-05-01 14:32:37 -0300

    Design Resposivo 768px na Apresentação

--- src/components/Apresentacao/style.ts
+++ src/components/Apresentacao/style.ts
@@ -10,17 +10,31 @@ export const Container = styled.div`
   align-items: center;
   justify-content: space-around;
   font-family: "Open Sans", sans-serif;
+
+  @media (max-width: 768px) {
+    width: 80%;
+  }
 `;
 
 export const Texto = styled.div`
   min-height: 200px;
   color: #fff;
+
+  @media (max-width: 768px) {
+    p {
+      font-size: 12px;
+    }
+  }
 `;
 
 export const Title = styled.h1`
   color: #bb6159;
   font-size: 48px;
   margin-bottom: 10px;
+
+  @media (max-width: 768px) {
+    font-size: 35px;
+  }
 `;
 
 export const Link = styled.a`
@@ -41,6 +55,13 @@ export const Link = styled.a`
     color: #122a57;
     transition: 0.3s ease all;
   }
+
+  @media (max-width: 768px) {
+    font-size: 15px;
+    width: 150px;
+    height: 35px;
+    line-height: 35px;
+  }
 `;
 
 export const Image = styled.img`
@@ -49,4 +70,8 @@ export const Image = styled.img`
   border-radius: 50px;
   -webkit-box-reflect: below 5px
     linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.4));
+  @media (max-width: 768px) {
+    height: 200px;
+    width: 300px;
+  }
 `;
