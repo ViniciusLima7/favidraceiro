c5a5d4cf02b0552109cffb26b1afbe392f66847a
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-12-06 14:25:43 -0300

    fix: Corrige Nome da Empresa

--- src/components/Apresentacao/index.tsx
+++ src/components/Apresentacao/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Container, Image, Link, Texto, Title } from './style'
-import bannerVidro from '../../assets/vidro.jpg';
- const Apresentacao = () => {
+import React from "react";
+import { Container, Image, Link, Texto, Title } from "./style";
+import bannerVidro from "../../assets/vidro.jpg";
+const Apresentacao = () => {
+  return (
+    <Container>
+      <Texto>
+        <Title>
+          F.A <br /> VIDRACEIRO
+        </Title>
+        <p>O melhor serviço pra você.</p>
+        <p>Trabalhamos com vidros de qualidade.</p>
+        <Link href="#servicos">Saiba mais !</Link>
+      </Texto>
+      <div>
+        <div>
+          <Image src={bannerVidro} alt="Banner Vidro" />
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-    return (
-        <Container>
-            <Texto>
-                <Title>Blindes <br/> Vidros</Title>
-                <p>O melhor serviço pra você.</p>
-                <p>Trabalhamos com vidros Blindex.</p>
-                <Link href="#servicos">Saiba mais !</Link>
-            </Texto>
-            <div>
-                <div>
-                    <Image src={bannerVidro} alt="Banner Vidro" />
-                </div>
-            </div>
-            
-        </Container>
-    );
- }
-
- export default  Apresentacao;
+export default Apresentacao;
